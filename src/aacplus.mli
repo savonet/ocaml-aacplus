c145c20cdec2e35f278e6b76ae8d86d5d33c63b6
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2010-10-18 14:28:08 +0000

    Added doc && headers to ocaml-flac and ocaml-aacplus
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407831

--- src/aacplus.mli
+++ src/aacplus.mli
@@ -1,5 +1,5 @@
 (*
- * Copyright 2003-2009 Savonet team
+ * Copyright 2003-2010 Savonet team
  *
  * This file is part of Ocaml-aacplus.
  *
@@ -18,16 +18,66 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
- (** OCaml bindings for the libaacplus. *)
+ (** AAC+ encoding module for OCaml *)
 
+(** {3 Usage} *)
+
+(** Typical use of the encoding modules is:
+  * 
+  * {v  (* Define the encoding parameters: *)
+  * let channels = (..) in
+  * let samplerate = (..) in
+  * (* Bitrate is in bps (e.g. 64000) *)
+  * let bitrate = (..) in 
+  * (* Create an encoder *)
+  * let enc = Aacplus.create ~channels ~samplerate ~bitrate () in
+  * (* Get the frame size *)
+  * let samples = Aacplus.frame_size enc in
+  * (* Encode some data *)
+  * let data = (..some float array array value..) in
+  * let ret = Aacplus.encode enc data in
+  * (..do something with encoded data..)
+  * (..repeat encoding process..) v}
+  * 
+  * Remarks:
+  * - Documentation about valid and invalid configuration
+  *   parameters is currently not available.
+  * - See documentations for [encode] function about data
+  *   submited for encoding. *)
+
+(** {3 Types} *)
+
+(** Type of an encoder *) 
 type t
 
+(** {3 Exceptions} *)
+
+(** Raised when submiting invalid data for 
+  * encoding *)
 exception Invalid_data
+
+(** Raised when a requesting an invalid 
+  * encoding configuration. *)
 exception Invalid_config
 
+(** {3 Functions} *)
+
+(** Create an encoder 
+  *
+  * Raises [Invalid_config] if encoding parameters
+  * cannot be used by the library. *)
 val create : channels:int -> samplerate:int -> bitrate:int -> unit -> t
 
+(** Return the number of samples required for 
+  * each channel of data submited for encoding *)
 val frame_size : t -> int
 
+(** Encode some audio data. 
+  * 
+  * Raises [Invalid_data] if submited data
+  * does not have the number of channels given
+  * when initiating the encoder or does not have
+  * the number of samples returned by [frame_size]
+  * in each channel. *)
 val encode : t -> float array array -> string
 
